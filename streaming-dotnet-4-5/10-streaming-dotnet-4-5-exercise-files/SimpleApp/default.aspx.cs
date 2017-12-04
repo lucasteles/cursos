53a4510ee6fd2ef66ae071b9c8352273a85775fb
Lucas Teles <lucas.teles@lambda3.com.br>
2017-12-04 16:01:08 -0200

    straming

---
+++ streaming-dotnet-4-5/10-streaming-dotnet-4-5-exercise-files/SimpleApp/default.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleApp
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        
+    }
+}
